0c65098a41ec66258ac0e8df0cb6ed4dae085e61
Kyle Cilia <kylecilia@gmail.com>
2014-01-30 17:00:11 -0800

    New base file
    Using Katie's base file.

--- daintyknight.js
+++ daintyknight.js
@@ -1,41 +1,42 @@
 //Initialize enchant.js as the game engine
-
-//Test Katie
-
-//test for second commit
 enchant();
-//use2D = true;
+use2D = true;
+
+
+
 //on document load [...]
-window.onload = function(){
+window.onload = function () {
 	//Starting point. Creating a variable called game as the game.
-	var game = new Game(1000,600); 	
-	//set keybinds	
+	var game = new Game(1000, 600);
+    //set keybinds	
 	game.keybind(37, 'left');		//Left arrow key
 	game.keybind(39, 'right');		//Right arrow key
 	//Preload resources to be used later on. Any not preloaded resources will
 	//not appear.
-	game.preload('sprites/bg.png');		
-	game.preload('sprites/sprite_neutral.png');
+	game.preload('sprites/bg.png');
+    game.preload('sprites/sprite_neutral.png');
 	game.preload('sprites/sprite_runL.png');
 	game.preload('sprites/sprite_runR.png');
 	//Game settings. These cannot be changed after the game has loaded. Set                   
 	//them before game.start() is called.
-	game.fps = 30;	
-	game.scale = 1;		
-	//on game load [...]
-	game.onload = function(){ 					
-		console.log("Dainty Knight! A game by Cake^3");
-		var scene, label, bg, knight;		
-		//Create new scene
+	game.fps = 30;
+    game.scale = 1;
+    //on game load [...]
+	game.onload = function () {
+        console.log("Dainty Knight! A game by Cake^3");
+		var scene, label, bg, knight;
+        //Create new scene
 		scene = new Scene();
 		//Creates new label
-		label = new Label("Dainty Knight!");	
+		label = new Label("Dainty Knight!");
 		//Creates new background sprite
-		bg = new Sprite(1000,600);				
+		bg = new Sprite(1000, 600);				
 		//Creates knight sprite starting at 40,500 coordinates
-		knight = new Sprite(64,64);
-		knight.x = 40;
-		knight.y = 500;
+		knight = new Sprite(64, 64);
+        knight = Knight(40, 500);
+		//knight.x = 40;
+		//knight.y = 500;
+        //knight = new Knight(40,500);
 		var playerSpeed = 4;
 		//Event listener for the left and right keys. Moves the player knight
 		//left or right.
@@ -52,8 +53,6 @@ window.onload = function(){
 		});
 		//Load background image
 		bg.image = game.assets['sprites/bg.png'];
-		//Load knight sprite
-		knight.image = game.assets['sprites/sprite_neutral.png'];
 		//Appends background and label to scene as children
 		scene.addChild(bg);			
 		scene.addChild(label);		
@@ -64,5 +63,15 @@ window.onload = function(){
 	}
 	//Initializes game
 	game.start();
+
+//knigt "class"
+function Knight (x, y) {
+    var knight = new Sprite(64, 64);
+    knight.x = x;
+    knight.y = y;
+    //load knight sprite
+    knight.image = game.assets['sprites/sprite_neutral.png'];
+    return knight;
 }
 
+};
